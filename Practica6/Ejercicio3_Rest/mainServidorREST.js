dfcd0b877542127100b2d6af00a0f8844f2b7fc6
Josue Bellota Ichaso <69655184+JosueBellota@users.noreply.github.com>
2025-05-24 19:46:31 +0200

    Ejercicio3 Servidor Rest copiado correctamente

---
+++ Practica6/Ejercicio3_Rest/mainServidorREST.js
@@ -0,0 +1,42 @@
+// .....................................................................
+// mainServidorREST.js
+// .....................................................................
+const express = require( 'express' )
+const bodyParser = require( 'body-parser' )
+
+
+// .....................................................................
+// main()
+// .....................................................................
+function main() {
+
+    // creo el servidor
+    var servidorExpress = express()
+
+
+    // para poder acceder a la carga de la petición http, asumiendo que es JSON
+    servidorExpress.use ( bodyParser.text({type : 'application/json'}) )
+    // cargo las reglas REST
+
+    
+    var reglas = require( "./ReglasREST.js")
+
+
+    reglas.cargar( servidorExpress )
+    // arrancao el servidor
+    var servicio = servidorExpress.listen( 8080, function() {
+
+        console.log( "servidor REST escuchando en el puerto 8080 ")
+    })
+    // capturo control-c para cerrar el servicio ordenadamente
+    process.on('SIGINT', function() {
+
+        console.log (" terminando ")
+        servicio.close ()
+    })
+} // ()
+// .....................................................................
+// .....................................................................
+main()
+// .....................................................................
+// .....................................................................
